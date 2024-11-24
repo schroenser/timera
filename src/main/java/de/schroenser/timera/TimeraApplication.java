7cd2a378bd8161d760b4c53ecafede6d51455ed3
Sven Haberer <schroenser@gmail.com>
2024-11-24 19:22:59 +0100

    Added a simple Spring Boot application with actuator support, that is packaged as a Docker image.

---
+++ src/main/java/de/schroenser/timera/TimeraApplication.java
@@ -0,0 +1,13 @@
+package de.schroenser.timera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeraApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TimeraApplication.class, args);
+    }
+}
