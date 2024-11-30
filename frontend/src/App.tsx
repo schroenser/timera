b7a12bd93fd9a5c5a0a68a07c4757a4b39fef70f
Sven Haberer <schroenser@gmail.com>
2024-11-30 16:30:24 +0100

    Calendar is now showing worklogs for the current week.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,31 @@
 import "./App.css";
+import {useEffect, useState} from "react";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import {Calendar, momentLocalizer} from "react-big-calendar";
+import {Calendar, Event, momentLocalizer} from "react-big-calendar";
 import moment from "moment";
+import {Worklog} from "./worklog";
 
 function App() {
     const localizer = momentLocalizer(moment);
 
+    let [events, setEvents] = useState<Event[]>([]);
+
+    useEffect(() => {
+        fetch("/api/worklog")
+        .then(response => response.json())
+        .then(json => json as Worklog[])
+        .then(worklogs => worklogs.map(worklog => {
+            return {
+                start: moment(worklog.started).toDate(),
+                end: moment(worklog.started).add(worklog.timeSpentSeconds, "seconds").toDate(),
+                title: worklog.issueSummary
+            };
+        }))
+        .then(events => setEvents(events));
+    }, [setEvents]);
+
     return (
-        <Calendar localizer={localizer} view="week" views={["week"]}/>
+        <Calendar localizer={localizer} view="week" views={["week"]} events={events}/>
     );
 }
 
---
+++ frontend/src/worklog.ts
@@ -0,0 +1,9 @@
+export type Worklog = {
+    issueId: string,
+    worklogId: string,
+    issueKey: string,
+    issueSummary: string,
+    started: string,
+    timeSpentSeconds: number,
+    worklogComment: string
+}
