96924d0366a3044bb3c53888a14846f445b3ec12
Sven Haberer <schroenser@gmail.com>
2024-11-30 15:38:17 +0100

    Added react-big-calendcar.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,13 @@
 import "./App.css";
-import {useEffect, useState} from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import {Calendar, momentLocalizer} from "react-big-calendar";
+import moment from "moment";
 
 function App() {
-    let [content, setContent] = useState("");
-
-    useEffect(() => {
-        fetch("/api/v1/hello")
-        .then(response => response.text())
-        .then(text => setContent(text));
-    }, [setContent]);
+    const localizer = momentLocalizer(moment);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>{content}</p>
-            </header>
-        </div>
+        <Calendar localizer={localizer}/>
     );
 }
 
