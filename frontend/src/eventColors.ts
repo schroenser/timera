e4e2a3ce030556f3207dbf995567dc93d3c0e9be
Sven Haberer <s.haberer@eurodata.de>
2024-12-05 10:38:35 +0100

    Event colors are now picked more deliberately from a fixed palette.

--- frontend/src/eventColors.ts
+++ frontend/src/eventColors.ts
@@ -1,41 +1,69 @@
 import {Event} from "react-big-calendar";
 
-const calculateHash = (string: string) => {
-    let hash = 0;
-    for (let i = 0; i < string.length; i++) {
-        hash =
-            string.charCodeAt(i) +
-            (
-                (
-                    hash << 5
-                ) - hash
-            );
-        hash = hash & hash;
+const palette = [
+    {
+        color: "#ffffff",
+        backgroundColor: "#e6194B"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#3cb44b"
+    }, {
+        color: "#000000",
+        backgroundColor: "#ffe119"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#4363d8"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#f58231"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#911eb4"
+    }, {
+        color: "#000000",
+        backgroundColor: "#42d4f4"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#f032e6"
+    }, {
+        color: "#000000",
+        backgroundColor: "#bfef45"
+    }, {
+        color: "#000000",
+        backgroundColor: "#fabed4"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#469990"
+    }, {
+        color: "#000000",
+        backgroundColor: "#dcbeff"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#9A6324"
+    }, {
+        color: "#000000",
+        backgroundColor: "#fffac8"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#800000"
+    }, {
+        color: "#000000",
+        backgroundColor: "#aaffc3"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#808000"
+    }, {
+        color: "#000000",
+        backgroundColor: "#ffd8b1"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#000075"
+    }, {
+        color: "#ffffff",
+        backgroundColor: "#a9a9a9"
     }
-    return hash;
-};
-
-const inRange = (hash: number, min: number, max: number) => {
-    const diff = max - min;
-    const x = (
-        (
-            hash % diff
-        ) + diff
-    ) % diff;
-    return x + min;
-};
-
-const toHsl = (string: string) => {
-    const hash = calculateHash(string);
-    const h = inRange(hash, 0, 360);
-    const s = inRange(hash, 40, 60);
-    const l = inRange(hash, 40, 60);
-    return `hsl(${h}, ${s}%, ${l}%)`;
-};
+];
 
 export function eventColors(event: Event) {
-    return {
-        color: "white",
-        backgroundColor: toHsl(event.resource.issueKey as string)
-    };
+    return palette[event.resource.issueId % palette.length];
 }
