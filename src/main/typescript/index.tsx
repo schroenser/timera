a7952956417b17eef4006f59f46a530b597110a5
Sven Haberer <schroenser@gmail.com>
2025-03-09 20:10:12 +0100

    (wip)

--- src/main/typescript/index.tsx
+++ src/main/typescript/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import "./index.css";
 import "@mantine/core/styles.css";
 import {MantineProvider} from "@mantine/core";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<React.StrictMode>
-    <MantineProvider>
-        <App/>
-    </MantineProvider>
-</React.StrictMode>);
+let domNode: HTMLElement = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (!domNode) {
+        domNode = document.getElementById("root");
+        const root = createRoot(domNode);
+        root.render(<React.StrictMode>
+            <MantineProvider>
+                <App/>
+            </MantineProvider>
+        </React.StrictMode>);
+    }
+});
